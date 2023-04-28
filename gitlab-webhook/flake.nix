f3a2407e5881d330167a152668b2f37d4219b44c
Bryan Richter <bryan@haskell.foundation>
2023-04-28 20:26:15 +0300

    Pass nix flake check

--- gitlab-webhook/flake.nix
+++ gitlab-webhook/flake.nix
@@ -5,14 +5,14 @@
       hsOverlay = pkgs: self: super: {
         spuriobot = self.callCabal2nix "spuriobot" ./. {};
       };
-      pkgs = import nixpkgs { system = "x86_64-linux"; overlays = [ self.overlay ]; };
+      pkgs = import nixpkgs { system = "x86_64-linux"; overlays = [ self.overlays.default ]; };
     in {
-      overlay = self: super: {
-        myHaskellPackages = super.haskellPackages.override {
-          overrides = hsOverlay self;
+      overlays.default = final: prev: {
+        myHaskellPackages = prev.haskellPackages.override {
+          overrides = hsOverlay final;
         };
 
-        myShell = self.myHaskellPackages.shellFor {
+        myShell = final.myHaskellPackages.shellFor {
           packages = pkgs: [ pkgs.spuriobot ];
         };
       };
@@ -23,14 +23,11 @@
           serviceConfig.ExecStart = pkgs.lib.getExe pkgs.myHaskellPackages.spuriobot;
         };
       };
-      devShell.x86_64-linux = pkgs.myShell;
-      packages.x86_64-linux.spuriobot = pkgs.myHaskellPackages.spuriobot;
-      apps.x86_64-linux.spuriobot = {
+      devShells.x86_64-linux.default = pkgs.myShell;
+      packages.x86_64-linux.default = pkgs.myHaskellPackages.spuriobot;
+      apps.x86_64-linux.default = {
         type = "app";
-        program = self.packages.x86_64-linux.spuriobot;
+        program = pkgs.lib.getExe self.packages.x86_64-linux.default;
       };
-
-      # defaultPackage.x86_64-linux = self.packages.x86_64-linux.spuriobot;
-      # defaultApp.x86_64-linux = self.apps.x86_64-linux.spuriobot;
     };
 }
