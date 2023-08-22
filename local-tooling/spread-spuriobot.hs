3180d81086b46007f16a1eff244dc299d91139e0
Bryan Richter <bryan@haskell.foundation>
2023-08-22 10:35:59 +0300

    Init spread-spuriobot and rename package

---
+++ local-tooling/spread-spuriobot.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module Main (main) where
+
+import Data.Aeson
+import Data.Text (Text)
+import Data.Word
+import Network.HTTP.Req
+import System.Environment
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+
+data Project = Project 
+    { id :: Word64
+    , path :: Text
+    } deriving (Eq, Show)
+
+instance FromJSON Project where
+    parseJSON = withObject "Project" $ \o ->
+        Project
+            <$> o .: "id"
+            <*> o .: "path_with_namespace"
+
+main :: IO ()
+main = do
+    strApiToken <- T.encodeUtf8 . T.pack <$> getEnv "GITLAB_API_TOKEN"
+    let url = https "gitlab.haskell.org" /: "api" /: "v4" /: "projects"
+    ps :: [Project] <- fmap responseBody $ runReq defaultHttpConfig $ req
+        GET
+        url
+        NoReqBody
+        jsonResponse
+        ("simple" =: True <> "per_page" =: (100 :: Word) <> headerRedacted "PRIVATE_TOKEN" strApiToken)
+    print ps
